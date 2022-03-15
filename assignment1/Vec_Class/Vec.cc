01611614f391313e6aa48ab1a031e36b00e34508
Grant <grant.brown@outlook.com>
2022-03-16 08:14:37 +1030

    Added push_back() and grow() methods in Vec.cc, created and tested exe with makefile. Tested OK.

--- assignment1/Vec_Class/Vec.cc
+++ assignment1/Vec_Class/Vec.cc
@@ -1,6 +1,7 @@
 // Vec.cc
 #include "Vec.h"
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
 Vec::Vec()
@@ -29,7 +30,8 @@ Vec::Vec(const Vec& vec_to_copy)
 	pointer_to_data = new double[array_limit];
 
 	// Copy data across.
-	for (int i = 0; i != array_current_size; ++i) {
+	for (int i = 0; i != array_current_size; ++i)
+	{
 		pointer_to_data[i] = vec_to_copy.pointer_to_data[i];
 	}
 }
@@ -41,7 +43,7 @@ Vec::~Vec()
 	// Set pointer to null
 	pointer_to_data = 0;
 	array_current_size = array_limit = 0;
-} 
+}
 // Overloading the assignment operator.
 Vec& Vec::operator=(const Vec& rhs)
 {
@@ -59,7 +61,7 @@ Vec& Vec::operator=(const Vec& rhs)
 		for (int i = 0; i != array_current_size; ++i) {
 			pointer_to_data[i] = rhs.pointer_to_data[i];
 		}
-	}		
+	}
 
 	return *this;
 }
@@ -67,4 +69,71 @@ Vec& Vec::operator=(const Vec& rhs)
 double& Vec::operator[](int i)
 {
 	return pointer_to_data[i];
+}
+
+// Method to dynamically increase* the size of an array at runtime. 
+//								*Create new array with more space and copy old array contents to it. 
+void Vec::grow()
+{
+	/*
+	* Account for zero and negative array_limits (limit of elements allowed in current array)
+	*/
+	//is the array size limit negative or zero
+	if (array_limit <= 0)
+	{
+		//if so, delete allocated memory
+		delete[] pointer_to_data;
+
+		//set the array size limit to a value > 0
+		array_limit = 1;
+
+		//Initialise array with the initialised size limit in memory
+		pointer_to_data = new double[array_limit];
+	}
+	//is the array size limit > zero
+	else
+	{
+		//create a storage array to hold data member values in memory
+		double* temp_array = new double[array_limit];
+
+		//double our array size limit
+		array_limit *= 2;
+
+		//iterate through our temporary array		
+		for (int i = 0; i < get_current_size(); i++)
+		{
+			//assign values of our current array to the temporary array
+			temp_array[i] = pointer_to_data[i];
+		}
+
+		//remove pointer_to_data from memory
+		delete[] pointer_to_data;
+
+		//create an array with new size limits in memory
+		pointer_to_data = new double[array_limit];
+
+		//iterate through our new array
+		for (int i = 0; i < get_current_size(); i++)
+		{
+			//assign values of our temporary array to our new array
+			pointer_to_data[i] = temp_array[i];
+		}
+
+		//remove temp_array from memory
+		delete[] temp_array;
+	}
+}
+
+//Method to "push" a variable ('a') to the "back" of an array
+void Vec::push_back(const double& a)
+{
+	//if current array is full, call grow() to allow additional elements to be added to the array
+	if (array_current_size >= array_limit)
+		grow();
+
+	//increment array_current_size
+	array_current_size++;
+
+	//append 'a' onto end of array
+	pointer_to_data[array_current_size - 1] = a;
 }
